f66c98512a88d3fce6ce4b06a2caef3ba39b248b
Johnaven <johnavenfreelance@gmail.com>
2022-02-19 12:06:38 -0500

    Fix bug where view.js would sometimes not be recognzied by listeners.js when game is first initialized.

--- listeners.js
+++ listeners.js
@@ -1,5 +1,5 @@
-import * as controller from "./controller.js";
 import * as view from "./view.js";
+import * as controller from "./controller.js";
 import { globalState } from "./state.js";
 
 let guard = false;
